dac04b16bfce0944ef02f52a529466d31273ded5
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-10-01 13:32:24 +0330

    Switch active-link class when scrolled

--- js/app.js
+++ js/app.js
@@ -14,3 +14,28 @@ function scrollHeader() {
 }
 
 window.addEventListener("scroll", scrollHeader);
+
+// scroll section active link
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 50,
+      sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+
+window.addEventListener("scroll", scrollActive);
