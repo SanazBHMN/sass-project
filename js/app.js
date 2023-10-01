d2016c5ff306c3ece78a3e0f2cbbecbc3662e926
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-10-01 13:12:58 +0330

    Add scroll listener

--- js/app.js
+++ js/app.js
@@ -5,3 +5,12 @@ const navToggle = document.querySelector("#nav-toggle");
 navToggle.addEventListener("click", function () {
   navMenu.classList.toggle("show-menu");
 });
+
+// apply shodow to header when scrolled
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+
+window.addEventListener("scroll", scrollHeader);
