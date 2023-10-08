ea87e76e8cf3fa5549449d22b4ec1ee17bb4f06f
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-10-08 17:57:47 +0330

    Implement scroll up

--- js/app.js
+++ js/app.js
@@ -46,3 +46,12 @@ function scrollActive() {
 }
 
 window.addEventListener("scroll", scrollActive);
+
+// show scroll up
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+
+window.addEventListener("scroll", scrollUp);
